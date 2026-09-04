9ed0f8a69f430e5b07ab7f76b1abe165f44fcfaf
nico-enred <nicoenred@pm.me>
2026-09-04 08:02:41 +0200

    Implement version check and update mechanism
    Added version checking and updating functionality.

--- test.py
+++ test.py
@@ -5,6 +5,70 @@ result=''
 list_guessing_letters=[]
 list_fails=[]
 list_ahorcado=['AHORCADO']
+# *********************************Programa Viejo*******************************
+import urllib.request
+import sys
+import subprocess
+# **************Comprobar versión del programa*************
+url_check_version=('https://raw.githubusercontent.com/nico-enred/Updater/refs/heads/main/Version_check')
+def check_version():
+    inside_url_origen=[]
+    fhand=urllib.request.urlopen(url_check_version)
+    for line in fhand:
+        line=line.decode().strip()
+        inside_url_origen.append(line)
+    fhand.close()
+    return inside_url_origen
+in_url_origen=check_version()#llamamos la función para tener su return disponible como parametro
+
+def extract_content_from_url_origin(in_url_origen):#extraígo el contenido de la url.
+    new_version=in_url_origen[0]#La nueva versión que está en la primera línea
+    downloading_new_file_url=in_url_origen[1]#La url del archivo nuevo para descargar
+    modifications=in_url_origen[3:]#Las modificaciones que contiene el nuevo programa. De la tercera línea en adelante
+    return new_version,downloading_new_file_url,modifications
+
+new_version,downloading_new_file_url,modifications=extract_content_from_url_origin(in_url_origen)#Se llama la función para tener disponibles
+                                                                                                #las variables
+modifications_='\n'.join(modifications)
+
+def get_current_version():#Obtenemos la versión actual del archivo del archivo current_version.txt
+    with open('current_version.txt','r') as fhand2:
+        for line in fhand2:
+            return line.strip()#it exists after reading the first line. if ever were added more lines the return should move out of the loop.
+        
+current_version=get_current_version()
+
+def change_to_new_version_in_currrent_version(new_version):#Cambiar version en current_version.txt
+    with open('current_version.txt','w') as fhand3:
+        fhand3.write(new_version)
+def download_new_file(downloading_new_file_url):#Descargar archivo nuevo desde la url que hay en la segunda posición
+    data=urllib.request.urlopen(downloading_new_file_url).read()
+    with open('New_Ahorcado.py','wb') as fhand4:
+        fhand4.write(data)
+# ***************terminate program and start new one***************************************
+def terminate_program(): #to be run on the program to delete after all the checks
+    sys.exit()
+    
+def start_updater():
+    subprocess.Popen(['python3','Updater_7.py'])#after checking there is a new version in the old program, it starts the updater
+def actualizar():
+    change_to_new_version_in_currrent_version(new_version)
+    download_new_file(downloading_new_file_url)
+    start_updater()
+    terminate_program()
+    
+def no_actualizar():
+    boton_yes.grid_remove()
+    boton_no.grid_remove()
+    
+identificador=None
+def brilli(boton):
+    global identificador
+    if boton.cget('fg_color')=='light green':
+        boton.configure(fg_color='green')
+    else:
+        boton.configure(fg_color='light green')
+    identificador=root.after(1000,brilli,boton)
 # *************
 def word_from_file():
     list=[]
@@ -103,8 +167,8 @@ import customtkinter as ctk
 root=ctk.CTk()
 root.title('The hangman')
 root.geometry('500x500')
-root.resizable(False,False)
-ctk.set_appearance_mode('light')
+root.resizable(True,True)
+ctk.set_appearance_mode('Dark')
 # ctk.set_default_color_theme('blue') es el color que tendrían los widgets si no defino yo uno al crearlos
 root.grid_columnconfigure(0,weight=1)
 # widget boton de inicio de juego
@@ -128,4 +192,23 @@ widget_label_hangman.grid(row=5,column=0,padx=20,pady=10,sticky='we')
 # boton de reiniciar juego
 widget_new_game=ctk.CTkButton(root,command=reset,text='Juego nuevo',fg_color='black',text_color='red',font=('Arial',20,'bold'),corner_radius=8,state='disabled')
 widget_new_game.grid(row=6,column=0, sticky='we',padx=150,pady=(100,0))
+if current_version!=new_version: #si se cumple la condición empezamos la actualización
+        frame_actualizar=ctk.CTkFrame(root,fg_color='light blue',border_color='blue',border_width=5)
+        frame_actualizar.grid_configure(row=0,column=0,sticky='nswe')
+        label_show_version=ctk.CTkLabel(frame_actualizar,text=f'La versión actual es:{current_version}. Hay una nueva versión disponible.\n¿Quiere actualizar?'
+                                        ,fg_color='green',border_color='yellow',border_width=2)
+        label_show_version.grid_configure(row=0,
+                                          column=0,columnspan=3,sticky='nswe')
+        label_show_modification=ctk.CTkLabel(frame_actualizar,text=f"Las modificaciones que se añadirán son:\n{modifications_}")
+        label_show_modification.grid_configure(row=1,
+                                               column=0,columnspan=3,sticky='nswe')
+        boton_yes=ctk.CTkButton(frame_actualizar,text='Actualizar',fg_color='light green',hover_color='green',command=actualizar)
+        boton_yes.grid_configure(row=2
+                                 ,column=0)
+        boton_no=ctk.CTkButton(frame_actualizar,text='No Actualizar',fg_color='pink',hover_color='red',command=no_actualizar)
+        boton_no.grid_configure(row=2,
+                                column=3)
+        frame_actualizar.tkraise()
+        brilli(boton_yes) 
 root.mainloop()
+
